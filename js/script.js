95f3dfd437a588f3865c4dc1e3f49f5d6dc69acf
skib4k <57643378+skib4k@users.noreply.github.com>
2021-01-20 18:13:14 +0200

    add mobile portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const icon = document.getElementsByClassName('navbar__icon');
+const item = document.getElementsByClassName('navbar__item');
+const list = document.querySelector('.navbar__list');
+
+for(i = 0; i < icon.length; i++) {
+    icon[i].addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if (list.classList.contains('navbar__item--active')) {
+            list.classList.remove('navbar__list--mobile');
+        } else {
+            list.classList.add('navbar__list--mobile');
+        }
+
+        for(i = 0; i < item.length; i++) {
+            item[i].classList.remove('navbar__item--active');
+        }
+        this.parentElement.classList.toggle('navbar__item--active');
+    });
+};
 
